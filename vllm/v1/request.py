a0eded6e5a375fdf1e73990d5d3145e20f122856
snowcharm <snowcharmqq@gmail.com>
2025-05-06 14:29:51 +0800

    fix useless task_emb

--- vllm/v1/request.py
+++ vllm/v1/request.py
@@ -23,7 +23,6 @@ class Request:
         request_id: str,
         prompt_token_ids: list[int],
         his_emb: Optional[torch.Tensor],
-        task_emb: Optional[torch.Tensor],
         multi_modal_inputs: Optional[list[MultiModalKwargs]],
         multi_modal_hashes: Optional[list[str]],
         multi_modal_placeholders: Optional[list[PlaceholderRange]],
@@ -41,7 +40,6 @@ class Request:
         self.lora_request = lora_request
         self.structured_output_request = structured_output_request
         self.his_emb = his_emb
-        self.task_emb = task_emb
 
         self.status = (RequestStatus.WAITING_FOR_FSM
                        if sampling_params.guided_decoding is not None else
@@ -88,7 +86,6 @@ class Request:
             request_id=request.request_id,
             prompt_token_ids=request.prompt_token_ids,
             his_emb=request.his_emb,
-            task_emb=request.task_emb,
             multi_modal_inputs=request.mm_inputs,
             multi_modal_hashes=request.mm_hashes,
             multi_modal_placeholders=request.mm_placeholders,
